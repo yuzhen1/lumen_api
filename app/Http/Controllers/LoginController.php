0621c8f188967fbf437b537bed69e34d4b961ada
jennifer <1793880774@qq.com>
2019-05-14 07:08:56 +0000

    debug

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,11 +11,6 @@ header("Access-Control-Allow-Origin: http://127.0.0.1:8848");
 class LoginController extends Controller{
     //注册
     public function register(request $request){
-
-//        $user_email=$request->input('user_email');
-//        echo $user_email;die;
-//        echo "<pre>";print_r($_POST);echo "</pre>";die;
-//        $data = file_get_contents('php://input');
         $data = file_get_contents('php://input');
         $data2 = base64_decode($data);
         $public_key = openssl_pkey_get_public('file://'.storage_path('/app/keys/public.pem'));
